692aa5f487d049b9b42362e9f3aa315341d1e4e0
hoangdtcodegym <hoang.du@codegym.vn>
2021-07-27 14:14:12 +0700

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('products');
+    }
+
+    public function cart()
+    {
+        return view('cart');
+    }
+    public function addToCart($id)
+    {
+
+    }
+}
